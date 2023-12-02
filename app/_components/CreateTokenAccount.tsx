ac0dc2ab37b0a21fbb1d01d790256815a6a71ad0
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2023-12-02 09:24:35 +0000

    :sparkles: create token account

--- app/_components/CreateTokenAccount.tsx
+++ app/_components/CreateTokenAccount.tsx
@@ -7,23 +7,82 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import { PublicKey, Transaction } from "@solana/web3.js";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+} from "@solana/spl-token";
 
 export const CreateTokenAccount: FC = () => {
+  const [txSig, setTxSig] = useState("");
   const [mint, setMint] = useState("");
   const [owner, setOwner] = useState("");
   const [tokenAccount, setTokenAccount] = useState("");
 
   const { connection } = useConnection();
-  const { publicKey } = useWallet();
+  const { publicKey, sendTransaction } = useWallet();
+  const link = () => {
+    return txSig
+      ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet`
+      : "";
+  };
 
-  const handleMintFieldChange = function () {};
+  useEffect(() => {
+    if (!connection || !publicKey) {
+      return;
+    }
 
-  const handleOwnerFieldChange = function () {};
+    setOwner(publicKey.toString());
+  }, [connection, publicKey]);
 
-  const handleCreateTokenAccountButton = async function () {};
+  const handleMintFieldChange = function (event: any) {
+    const { target } = event;
+    setMint(target.value);
+  };
+
+  const handleOwnerFieldChange = function (event: any) {
+    const { target } = event;
+    setOwner(target.value);
+  };
+
+  const handleCreateTokenAccountButton = async function () {
+    if (!connection || !publicKey) {
+      return;
+    }
+
+    const mintPubkey = new PublicKey(mint);
+    const ownerPubkey = new PublicKey(owner);
+
+    const associatedTokenAddress = await getAssociatedTokenAddress(
+      mintPubkey,
+      ownerPubkey,
+      false,
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    );
+
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        publicKey,
+        associatedTokenAddress,
+        ownerPubkey,
+        mintPubkey,
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      )
+    );
+
+    sendTransaction(transaction, connection).then((sig) => {
+      setTxSig(sig);
+      setTokenAccount(associatedTokenAddress.toString());
+    });
+  };
 
   return (
-    <Card sx={{ flexGrow: 1, m: 2  }}>
+    <Card sx={{ flexGrow: 1, m: 2 }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Create a New Token Account
@@ -46,13 +105,22 @@ export const CreateTokenAccount: FC = () => {
           onChange={handleOwnerFieldChange}
           value={owner}
         />
-        <Button onClick={handleCreateTokenAccountButton}>
-          Create Token Account
-        </Button>
-        {tokenAccount && (
-          <Typography variant="h5" component="div">
-            {tokenAccount}
-          </Typography>
+        {publicKey && !txSig ? (
+          <Button onClick={handleCreateTokenAccountButton}>
+            Create Token Account
+          </Button>
+        ) : publicKey && txSig ? null : (
+          <Typography>Connect Your Wallet</Typography>
+        )}
+
+        {txSig && (
+          <>
+            <Typography>Token Account: {tokenAccount}</Typography>
+            <Typography>View your transaction on</Typography>
+            <Typography>
+              <Link href={link()}>Solana Explorer</Link>
+            </Typography>
+          </>
         )}
       </CardContent>
     </Card>
